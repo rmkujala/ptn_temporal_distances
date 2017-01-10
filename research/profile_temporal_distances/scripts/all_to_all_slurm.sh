a49671e62f03bdc0802511dbca660a82a9bba1fc
Rainer Kujala <rainer.kujala@gmail.com>
2017-01-10 14:51:38 +0200

    update slurm script for all_to_all

--- research/profile_temporal_distances/scripts/all_to_all_slurm.sh
+++ research/profile_temporal_distances/scripts/all_to_all_slurm.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #SBATCH -n 1
-#SBATCH -t 04:00:00
-#SBATCH ---mem-per-cpu=2500
-#SBATCH --array=0-1
+#SBATCH -t 02:00:00
+#SBATCH ---mem-per-cpu=2500M
+#SBATCH --array=0-127
 
 #SBATCH
-srun python all_to_all_stats.py $SLURM_ARRAY_TASK_ID 1000
+srun python all_to_all_stats.py $SLURM_ARRAY_TASK_ID 128
